53388d3bca550ac2f122dd3b9972120ed1c3a155
Akramjon <akramjon00175@gmail.com>
2023-05-22 09:13:21 -0700

    Initial Commit

---
+++ Shop Automatization/Shop_AutomatizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_AutomatizationApp.swift
+//  Shop Automatization
+//
+//  Created by Akramjon on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Shop_AutomatizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
